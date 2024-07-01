8eaff30d190b5bca4f6febe52117f6e8696c1a49
Gokhan Kus <gkus1998@hotmail.com>
2024-07-01 15:32:56 +0300

    AddAfter(), AddBefore() methods

--- DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -8,11 +8,12 @@ namespace DataStructuresLibrary.LinkedList.SinglyLinkedList
 {
 	public class SinglyLinkedList<T>
 	{
-		public SinglyLinkedListNode<T> Head { get; set; } //ilk node'umuz, ilk degerimiz
+		public SinglyLinkedListNode<T>? Head { get; set; } //ilk node'umuz, ilk degerimiz
+        private bool isHeadNull => Head == null;
 
-		//veri eklerken ilk dugume ekleme metodu Head kısmı degisir big O(1)
-		//bu metot her zaman listenin basina ekleme yapar
-		public void AddFirst(T value)
+        //veri eklerken ilk dugume ekleme metodu Head kısmı degisir big O(1)
+        //bu metot her zaman listenin basina ekleme yapar
+        public void AddFirst(T value)
 		{
 			var newNode = new SinglyLinkedListNode<T>(value); //parametre olarak gelen deger ile yeni node olusturuluyor ve next propu(pointer) null oluyor
 			newNode.Next = Head; //bu satirda yeni eklenen dugume (newNode) headi isaret etmesini soyluyoruz
@@ -22,7 +23,7 @@ namespace DataStructuresLibrary.LinkedList.SinglyLinkedList
 		public void AddLast(T value)
 		{
 			var newNode = new SinglyLinkedListNode<T>(value);
-			if (Head == null) //listede hic eleman olmayabilir
+			if (isHeadNull) //listede hic eleman olmayabilir
 			{
 				Head = newNode;
 				return;
@@ -33,6 +34,66 @@ namespace DataStructuresLibrary.LinkedList.SinglyLinkedList
 				current = current.Next;
 			}
 			current.Next = newNode;
+			LinkedList<int> asd = new LinkedList<int>();
+		}
+		//hangi dugumden sonra bir dugum eklemek istiyoruz? big O(n) araya ekleme yapar AddLast ile benzer ozellikte
+		public void AddAfter(SinglyLinkedListNode<T> refNode, T value)
+		{
+			if (refNode == null)
+				throw new ArgumentException("node cannot be null");
+
+			if (isHeadNull)
+			{
+				AddFirst(value);
+				return;
+			}
+
+			var newNode = new SinglyLinkedListNode<T>(value);
+			var current = Head;
+			while (current != null)
+			{
+				if (current == refNode)
+				{
+					newNode.Next = current.Next; //ekledigimiz node'un next'i kendisinden sonra gelen node'u gosterecek
+					current.Next = newNode; //burasi current'in, yani hangi node'dan sonra eklemek istiyorsak o node'un nexti, eklemek istedigimiz degeri gosterecek
+					return;
+				}
+				current = current.Next;
+			}
+			throw new ArgumentException("the reference node is not in this list");
+		}
+		
+		public void AddBefore(SinglyLinkedListNode<T> refNode, T value)
+		{
+			if (refNode == null)
+				throw new ArgumentException("node cannot be null");
+
+			if (isHeadNull)
+			{
+				AddFirst(value);
+				return;
+			}
+			var newNode = new SinglyLinkedListNode<T>(value);
+			var current = Head;
+			while (current.Next != null)
+			{
+				if (current.Next == refNode)
+				{
+					newNode.Next = current.Next;
+					current.Next = newNode;
+					return;
+				}
+				current = current.Next;
+			}
+			throw new ArgumentException("the reference node is not in this list");
+		}
+		public void AddBefore(SinglyLinkedListNode<T> refNode, SinglyLinkedListNode<T> newNode)
+		{
+			throw new NotImplementedException();
+		}
+		public void AddAfter(SinglyLinkedListNode<T> refNode, SinglyLinkedListNode<T> newNode)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,9 +10,15 @@ namespace LinkedList
 			linkedList.AddFirst(1);
 			linkedList.AddFirst(2);
 			linkedList.AddFirst(3);
-
+			//3 2 1 
 			linkedList.AddLast(4);
 			linkedList.AddLast(5);
+			//3 2 1 4 5
+			linkedList.AddAfter(linkedList.Head.Next.Next, 32);
+			//3 2 1 32 4 5
+			linkedList.AddBefore(linkedList.Head.Next.Next, 30);
+			//3 2 30 1 32 4 5
+
 		}
 	}
 }
