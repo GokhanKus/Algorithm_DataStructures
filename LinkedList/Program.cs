c2b78938f05ae64aa6a1acd3dc2b1d27736afc28
Gokhan Kus <gkus1998@hotmail.com>
2024-07-01 18:43:58 +0300

    LINQ using

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -6,6 +6,41 @@ namespace LinkedList
 	{
 		static void Main(string[] args)
 		{
+			//LinkedListApp1();
+			//LinkedListApp2();
+			LinkedListApp3();
+
+		}
+
+		private static void LinkedListApp3()
+		{
+			var rnd = new Random();
+			var initial = Enumerable.Range(1, 10).OrderBy(x => rnd.Next()).ToList();
+			var linkedList = new SinglyLinkedList<int>(initial);
+
+			var q = from item in linkedList
+					where item % 2 == 0
+					select item;
+
+			foreach (var item in q)
+			{
+				Console.WriteLine(item);
+			}
+
+			linkedList.Where(x => x > 5).ToList().ForEach(x => Console.Write(x + " "));
+		}
+		private static void LinkedListApp2()
+		{
+			var charList = new List<char> { 'a', 'b', 'c' };
+			var linkedList2 = new SinglyLinkedList<char>(charList);
+			foreach (var item in linkedList2)
+			{
+				Console.WriteLine(item);
+			}
+			var linkedList3 = new SinglyLinkedList<int>(1, 5, 11, 16);
+		}
+		private static void LinkedListApp1()
+		{
 			var linkedList = new SinglyLinkedList<int>();
 			linkedList.AddFirst(1);
 			linkedList.AddFirst(2);
@@ -20,15 +55,8 @@ namespace LinkedList
 			//3 2 30 1 32 4 5
 			foreach (var item in linkedList)
 			{
-                Console.WriteLine(item);
-            }
-			var charList = new List<char> { 'a', 'b', 'c' };
-			var linkedList2 = new SinglyLinkedList<char>(charList);
-			foreach (var item in linkedList2)
-			{
-                Console.WriteLine(item);
-            }
-			var linkedList3 = new SinglyLinkedList<int>(1, 5, 11, 16);
+				Console.WriteLine(item);
+			}
 		}
 	}
 }
