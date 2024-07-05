60c3c5bb679bc8289b9cf12a2fc87762d7b4d119
Gokhan Kus <gkus1998@hotmail.com>
2024-07-05 20:19:19 +0300

    PostFix Example

---
+++ DataStructures/Stack/PostFixExample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.Stack
+{
+	public class PostFixExample
+	{
+		private string expression;
+		CustomStack<string> stack = new CustomStack<string>();
+		private string Expression()
+		{
+			//"231*+9-"
+			int val1, val2, ans;
+			for (int i = 0; i < expression.Length; i++)
+			{
+				string c = expression[i].ToString();
+				if (c == "*")
+				{
+					//val1 = int.Parse(stack.Pop()); // '*' sembolunden onceki son iki sayiyi alalim ve stackten cikaralim
+					//val2 = int.Parse(stack.Pop());
+					GetTwoNumbersAndPop(out val1, out val2);
+					ans = val2 * val1;              //operand "*" oldugu icin carpip sonucu stack'e pushlayalım
+					stack.Push(ans.ToString());
+				}
+				else if (c == "/")
+				{
+					GetTwoNumbersAndPop(out val1, out val2);
+					ans = val2 / val1;
+					stack.Push(ans.ToString());
+				}
+				else if (c == "+")
+				{
+					GetTwoNumbersAndPop(out val1, out val2);
+					ans = val2 + val1;
+					stack.Push(ans.ToString());
+				}
+				else if (c == "-")
+				{
+					GetTwoNumbersAndPop(out val1, out val2);
+					ans = val2 - val1;
+					stack.Push(ans.ToString());
+				}
+				else
+				{
+					stack.Push(c);
+				}
+			}
+			return stack.Pop();
+		}
+		public static string Run(string expression)
+		{
+			PostFixExample postFix = new PostFixExample();
+			postFix.expression = expression;
+			return postFix.Expression();
+		}
+
+		private void GetTwoNumbersAndPop(out int val1, out int val2)
+		{
+			val1 = int.Parse(stack.Pop());
+			val2 = int.Parse(stack.Pop());
+		}
+	}
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -6,6 +6,22 @@ namespace Stack
 	{
 		static void Main(string[] args)
 		{
+			//StackApp1();
+			StackApp2PostFix();
+		}
+
+		private static void StackApp2PostFix()
+		{
+			//in fix => 3 + 5 = 8
+			//post fix 3 5 + = 8 
+			//stackler post fix uygulamalari icin kullanilir asagidaki metotta ornegi yapilmistir.
+			//sembolden onceki 2 rakam alinir sembole gore islemi yapilip stack'e pushlanir.
+			string result = PostFixExample.Run("231*+9-");
+			Console.WriteLine(result);
+		}
+
+		private static void StackApp1()
+		{
 			var charSet = new char[] { 'a', 'b', 'c', 'd', 'e' };
 			var stack1 = new CustomStack<char>(); //bizim olusturdugumuz stack yapisi bu stack List tipindedir belirtmezsek default bu..
 			var stack2 = new CustomStack<char>(StackType.LinkedList); //bizim olusturdugumuz stack yapisi bu stack linkedlist tipindedir
@@ -23,7 +39,7 @@ namespace Stack
 
 			Console.WriteLine($"stack1 Count: {stack1.Count}");
 			Console.WriteLine($"stack2 Count: {stack2.Count}");
-			
+
 			//bu islemlerden sonra;
 			//stack1 : a b c d generic list
 			//stack2 : d c b a linked list
