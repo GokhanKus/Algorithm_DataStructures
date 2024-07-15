a3de2c294efcfa34f91d5d59ccaa9d9b18a7237d
Gokhan Kus <gkus1998@hotmail.com>
2024-07-15 13:54:05 +0300

    BinaryHeap<T> class design

---
+++ DataStructures/Heap/BinaryHeap.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.Heap
+{
+	public abstract class BinaryHeap<T> : IEnumerable<T> //foreach ile kullanmak istedigimiz icin IEnumerable ..
+	{
+		public T[] HeapArray { get; private set; }
+		public int Count { get; private set; }
+		private int position;
+		public BinaryHeap()
+		{
+			HeapArray = new T[128];
+			position = 0;
+			Count = 0;
+		}
+		public BinaryHeap(int _size)
+		{
+			HeapArray = new T[_size];
+			position = 0;
+			Count = 0;
+		}
+		//0 based index, for tree; (lever ordered)
+		private int GetLeftChildIndex(int i) => i * 2 + 1;
+		private int GetRightChildIndex(int i) => i * 2 + 2;
+		private int GetParentIndex(int i) => (i - 1) / 2;
+		private bool HasLeftChild(int i) => GetLeftChildIndex(i) < position;
+		private bool HasRightChild(int i) => GetRightChildIndex(i) < position;
+		private bool IsRoot(int i) => i == 0; //i = 0 ise ilk elemandır yani koktur 
+		private T GetLeftChild(int i) => HeapArray[GetLeftChildIndex(i)];
+		private T GetRightChild(int i) => HeapArray[GetRightChildIndex(i)];
+		private T GetParent(int i) => HeapArray[GetParentIndex(i)];
+		public bool IsEmpty() => position == 0;
+		public T Peek() => IsEmpty() ? throw new Exception("Empty Heap!") : HeapArray[0];
+		public void Swap(int first, int second)
+		{
+			var temp = HeapArray[first];
+			HeapArray[first] = HeapArray[second];
+			HeapArray[second] = temp;
+		}
+		public void Add(T value)
+		{
+			if (position == HeapArray.Length) throw new IndexOutOfRangeException("overflow!");
+			position++;
+			Count++;
+			//HeapifyUp(); yeni eklenen degerin son indexe eklendikten sonra min heap veya max heap yapisina uymuyorsa yukari dogru parentlerle yer degistiririz..
+		}
+		public T DeleteMinMax()
+		{
+			if (position == 0) throw new Exception("Empty Heap!");
+			var temp = HeapArray[0];
+			HeapArray[0] = HeapArray[position - 1]; //son elemani koke yerlestiriyoruz
+			position--;
+			Count--;
+			//HeapifyDown(); min heap ve max heapte farklı calisacak (indisteki degerlerin yerlerini degistirerek min heap veya max heape gore duzenleyecegiz)
+			return temp;
+		}
+		protected abstract void HeapifyUp();
+		protected abstract void HeapifyDown();
+		public IEnumerator<T> GetEnumerator() => HeapArray.Take(position).GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+}
