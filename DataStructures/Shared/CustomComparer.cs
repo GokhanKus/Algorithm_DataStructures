ea364c0914ae5db4381f6e78cce7707877e62288
Gokhan Kus <gkus1998@hotmail.com>
2024-07-16 14:24:29 +0300

    CustomComparer.cs & SortDirection enum to merge minheap, maxheap, binaryheap classes

---
+++ DataStructures/Shared/CustomComparer.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructuresLibrary.Shared
+{
+	public class CustomComparer<T> : IComparer<T> where T : IComparable
+	{
+		private readonly bool isMax;
+		private readonly IComparer<T> _comparer;
+		public CustomComparer(SortDirection direction, IComparer<T> comparer)
+		{
+			isMax = direction == SortDirection.Descending; // eger azalan ise max heaptir ve isMax true doner
+			_comparer = comparer;
+		}
+		public int Compare(T? x, T? y)
+		{
+			return isMax == false ? compare(x, y) : compare(y, x);
+		}
+		private int compare(T? x, T? y) => _comparer.Compare(x, y);
+	}
+}
---
+++ DataStructures/Shared/SortDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.Shared
+{
+	public enum SortDirection
+	{
+		Ascending = 0, //artan (min heap)
+		Descending = 1, //azalan (max heap)
+	}
+}
