fc491cf9a5f7f0aa90242125e000151e5c4d6ae5
Gokhan Kus <gkus1998@hotmail.com>
2024-07-18 10:29:38 +0300

    DisjointSet class design

---
+++ DataStructures/DisJointSet/DisjointSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.DisJointSet
+{
+	public class DisjointSet<T> : IEnumerable<T>
+	{
+		private Dictionary<T, DisjointSet<T>> set = new Dictionary<T, DisjointSet<T>>();
+		public int Count { get; private set; }
+		public void MakeSet() //ayrik set olusturma
+		{
+
+		}
+		public T FindSet(T value)
+		{
+			return value;
+		}
+		public void Union(T valueA, T valueB) //ayrik 2 seti birlestirme
+		{
+
+		}
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ DataStructures/DisJointSet/DisjointSetNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.DisJointSet
+{
+	public class DisjointSetNode<T>
+	{
+		public T? Value { get; set; }
+		public int Rank { get; set; }
+		public DisjointSetNode<T>? Parent { get; set; }
+		public DisjointSetNode()
+		{
+
+		}
+		public DisjointSetNode(T value, int rank = 0)
+		{
+			Value = value;
+			Rank = rank;
+		}
+		//public DisjointSetNode(T value)
+		//{
+		//	Value = value;
+		//	Rank = 0;
+		//}
+		public override string ToString()
+		{
+			return $"{Value}";
+		}
+	}
+}
