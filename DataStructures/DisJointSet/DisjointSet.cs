b50e22bcf81e110d3f2f1ddaca1ab4abe7354426
Gokhan Kus <gkus1998@hotmail.com>
2024-07-18 10:47:18 +0300

    MakeSet() function

--- DataStructures/DisJointSet/DisjointSet.cs
+++ DataStructures/DisJointSet/DisjointSet.cs
@@ -9,11 +9,16 @@ namespace DataStructuresLibrary.DisJointSet
 {
 	public class DisjointSet<T> : IEnumerable<T>
 	{
-		private Dictionary<T, DisjointSet<T>> set = new Dictionary<T, DisjointSet<T>>();
+		private Dictionary<T, DisjointSetNode<T>> set = new Dictionary<T, DisjointSetNode<T>>();
 		public int Count { get; private set; }
-		public void MakeSet() //ayrik set olusturma
+		public void MakeSet(T value) //ayrik set olusturma
 		{
-
+			if (set.ContainsKey(value))
+				throw new Exception("the value has already been defined");
+			var newSet = new DisjointSetNode<T>(value); //rank default 0 
+			newSet.Parent = newSet; //ayrik set olarak kendini isaret ediyor ve kök eleman
+			set.Add(value, newSet); //setimize yeni olusturulan kok degerimizi ekleyelim
+			Count++;
 		}
 		public T FindSet(T value)
 		{
