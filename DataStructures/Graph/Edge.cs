bfcf65b0b9aa06158220592c6838192ec1043dbe
Gokhan Kus <gkus1998@hotmail.com>
2024-07-19 11:23:51 +0300

    Edge.cs design

---
+++ DataStructures/Graph/Edge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary.Graph
+{
+	public class Edge<T, C> : IEdge<T> where C : IComparable
+	{
+		public Edge(IVertex<T> target, C weight)
+		{
+			TargetVertex = target;
+			_weight = weight;
+		}
+		private object _weight;
+		public T TargetVertexKey => TargetVertex.Key;
+		public IVertex<T> TargetVertex { get; private set; }
+		public W Weight<W>() where W : IComparable
+		{
+			return (W)_weight;
+		}
+		public override string ToString()
+		{
+			return TargetVertex.Key.ToString();
+		}
+	}
+}
