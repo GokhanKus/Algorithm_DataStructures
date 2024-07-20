e122dbf057fcb3623a325f8884e64841c584ba50
Gokhan Kus <gkus1998@hotmail.com>
2024-07-20 16:18:27 +0300

    WeightedGraph implementation

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataStructuresLibrary.Graph.AdjacencySet;
+using System.Runtime.InteropServices;
 
 namespace Graph
 {
@@ -6,9 +7,46 @@ namespace Graph
 	{
 		static void Main(string[] args)
 		{
-			GraphApp1();
+			//GraphApp1();
+			GraphApp2();
 		}
+		private static void GraphApp2()
+		{
+			var weightedGraph = new WeightedGraph<char, double>(new char[] { 'A', 'B', 'C', 'D' });
+			weightedGraph.AddVertex('E');
 
+			weightedGraph.AddEdge('A', 'B', 1.2);
+			weightedGraph.AddEdge('A', 'D', 2.3);
+			weightedGraph.AddEdge('D', 'C', 5.5);
+
+			foreach (var vertex in weightedGraph.VertexesAsEnumerable)
+			{
+				Console.Write(vertex.Key + " => ");
+				foreach (var edge in vertex.Edges)
+				{
+					Console.Write($"  {vertex.Key} - {edge.Weight<double>()} - {edge.TargetVertexKey} ||");
+				}
+				Console.WriteLine();
+			}
+			//2 foreach de ayni
+			//foreach (var vertex in weightedGraph)
+			//{
+			//	Console.Write($"{vertex} => ");
+			//	foreach (var neighbourVertex in weightedGraph.Edges(vertex)) //weightedGraph.GetVertex(vertex)
+			//	{
+			//		var node = weightedGraph.GetVertex(neighbourVertex).GetEdge(weightedGraph.GetVertex(vertex));
+			//		Console.Write($"  {vertex} - {node.Weight<double>()} - {neighbourVertex}");
+			//	}
+			//	Console.WriteLine();
+			//}
+			Console.WriteLine("is there an edge between A and B ? {0}", weightedGraph.HasEdge('A', 'B') ? "Yes" : "No!");
+			Console.WriteLine("is there an edge between B and A ? {0}", weightedGraph.HasEdge('B', 'A') ? "Yes" : "No!");
+
+			Console.WriteLine("is there an edge between B and C ? {0}", weightedGraph.HasEdge('B', 'C') ? "Yes" : "No!");
+			Console.WriteLine("is there an edge between C and B ? {0}", weightedGraph.HasEdge('C', 'B') ? "Yes" : "No!");
+
+			Console.WriteLine($"the number of vertex: {weightedGraph.Count}");
+		}
 		private static void GraphApp1()
 		{
 			char[] vertexes = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
@@ -25,7 +63,7 @@ namespace Graph
 			var vertexD = graph.GetVertex('D');
 			foreach (var neighbourVertex in vertexD)
 			{
-				Console.WriteLine(neighbourVertex); 
+				Console.WriteLine(neighbourVertex);
 			}
 			//key = vertexlerin degeri 'A', 'B' gibi.. hangi dugum hangisiyle iliskili (has edge? between two vertex)
 			foreach (var key in graph)
