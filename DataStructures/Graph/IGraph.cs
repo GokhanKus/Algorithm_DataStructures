6ca7a802779b545e8e1fb9278224bf32df3c38fd
Gokhan Kus <gkus1998@hotmail.com>
2024-07-21 10:15:49 +0300

    Directed graph interfaces implementation

--- DataStructures/Graph/IGraph.cs
+++ DataStructures/Graph/IGraph.cs
@@ -1,4 +1,5 @@
-﻿namespace DataStructuresLibrary.Graph
+﻿
+namespace DataStructuresLibrary.Graph
 {
 	public interface IGraph<T> : IEnumerable<T>
 	{
@@ -15,16 +16,35 @@
 		void RemoveVertex(T key);
 		void RemoveEdge(T source, T dest);
 	}
-	public interface IVertex<T> : IEnumerable<T> 
+	public interface IVertex<T> : IEnumerable<T>
 	{
 		T Key { get; }
 		IEnumerable<IEdge<T>> Edges { get; }
 		IEdge<T> GetEdge(IVertex<T> targetVertex);
 	}
-	public interface IEdge<T>  
+	public interface IEdge<T>
 	{
 		T TargetVertexKey { get; }
 		IVertex<T> TargetVertex { get; }
 		W Weight<W>() where W : IComparable;
+	}
+
+	public interface IDirectedGraph<T> : IGraph<T>
+	{
+		new IDirectedVertex<T> ReferenceVertex { get; }
+		new IEnumerable<IDirectedVertex<T>> VertexesAsEnumerable { get; }
+		new IDirectedVertex<T> GetVertex(T key);
+	}
+	public interface IDirectedVertex<T> : IVertex<T>
+	{
+		IDirectedEdge<T> GetOutEdge(IDirectedVertex<T> targetVertex);
+		IEnumerable<IDirectedEdge<T>> OutEdges { get; }
+		IEnumerable<IDirectedEdge<T>> InEdged { get; }
+		int OutEdgesCount { get; }
+		int InEdgesCount { get; }
+	}
+	public interface IDirectedEdge<T> : IEdge<T>
+	{
+		new IDirectedGraph<T> TargetVertex { get; }
 	}
 }
