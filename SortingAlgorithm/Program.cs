4a6b11ef38b498dbc076d74bcbab68c991dcc4fa
Gokhan Kus <gkus1998@hotmail.com>
2024-07-24 21:27:30 +0300

    Selection Sort

---
+++ SortingAlgorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using DataStructuresLibrary.Shared;
+using DataStructuresLibrary.SortingAlgorithm;
+
+namespace SortingAlgorithm
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
+			SelectionSort.Sort(arr, SortDirection.Descending);
+			foreach (int i in arr)
+			{
+				Console.Write(i + "  ");
+			}
+
+			Console.WriteLine();
+
+			SelectionSort.Sort(arr, SortDirection.Ascending);
+			foreach (int i in arr)
+			{
+				Console.Write(i + "  ");
+			}
+			Console.ReadLine();
+		}
+	}
+}
