1429780a685dbfdaef77c957cb2e2f3199b416f5
Gokhan Kus <gkus1998@hotmail.com>
2024-07-25 11:10:42 +0300

    Insertion Sort

--- SortingAlgorithm/Program.cs
+++ SortingAlgorithm/Program.cs
@@ -8,13 +8,24 @@ namespace SortingAlgorithm
 		static void Main(string[] args)
 		{
 			//SelectionSortApp();
-			BubbleSortApp();
+			//BubbleSortApp();
+			InsertionApp();
+		}
+
+		private static void InsertionApp()
+		{
+			int[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
+			var sortedList = InsertionSort.Sort(arr,SortDirection.Ascending);
+			foreach (int i in sortedList)
+			{
+				Console.Write(i + "  ");
+			}
 		}
 
 		private static void BubbleSortApp()
 		{
 			int[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
-			BubbleSort.Sort(arr,SortDirection.Descending);
+			BubbleSort.Sort(arr, SortDirection.Descending);
 			foreach (int i in arr)
 			{
 				Console.Write(i + "  ");
