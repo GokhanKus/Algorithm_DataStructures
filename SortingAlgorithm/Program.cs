a9651ea17f83ca9c2c52238bd7b4d5e04ca36908
Gokhan Kus <gkus1998@hotmail.com>
2024-07-26 11:36:33 +0300

    Heap Sort

--- SortingAlgorithm/Program.cs
+++ SortingAlgorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using DataStructuresLibrary.Shared;
+﻿using DataStructuresLibrary.Heap;
+using DataStructuresLibrary.Shared;
 using DataStructuresLibrary.SortingAlgorithm;
 
 namespace SortingAlgorithm
@@ -11,7 +12,27 @@ namespace SortingAlgorithm
 			//BubbleSortApp();
 			//InsertionApp();
 			//QuickSortApp();
-			MergeSortApp();
+			//MergeSortApp();
+			HeapSortApp();
+		}
+
+		private static void HeapSortApp()
+		{
+			byte[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
+
+			Console.WriteLine("sorting by maxheap");
+            var maxHeap = new BinaryHeap<byte>(SortDirection.Descending, arr);
+			foreach (var item in maxHeap)
+			{
+				Console.Write($"{maxHeap.DeleteMinMax(),-5}");
+			}
+
+			Console.WriteLine("\nsorting by minheap");
+			var minHeap = new BinaryHeap<byte>(SortDirection.Ascending, arr);
+			foreach (var item in minHeap)
+			{
+				Console.Write($"{minHeap.DeleteMinMax(),-5}");
+			}
 		}
 
 		private static void MergeSortApp()
@@ -29,19 +50,19 @@ namespace SortingAlgorithm
 		private static void QuickSortApp()
 		{
 			int[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
-			
+
 			QuickSort.Sort(arr);
 			foreach (int i in arr)
 			{
 				Console.Write(i + "  ");
 			}
-            Console.WriteLine();
-        }
+			Console.WriteLine();
+		}
 
 		private static void InsertionApp()
 		{
 			int[] arr = { 10, 5, 15, 25, 0, 15, 75, 20 };
-			var sortedList = InsertionSort.Sort(arr,SortDirection.Ascending);
+			var sortedList = InsertionSort.Sort(arr, SortDirection.Ascending);
 			foreach (int i in sortedList)
 			{
 				Console.Write(i + "  ");
