edf1e54050ed594e7beee39f674768fdd2583982
Gokhan Kus <gkus1998@hotmail.com>
2024-08-09 17:52:56 +0300

    custom type defined

---
+++ CustomTypesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using CustomTypesLibrary;
+
+namespace CustomTypesApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var student = new Student(10, "Gokhan", 2.75);
+			Console.WriteLine(student);
+		}
+	}
+}
---
+++ CustomTypesLibrary/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomTypesLibrary
+{
+	public class Student
+	{
+
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public double GPA { get; set; }
+		public Student(int id, string name, double gpa)
+		{
+			Id = id;
+			Name = name;
+			GPA = gpa;
+		}
+        public Student()
+        {
+            
+        }
+        public override string ToString()
+		{
+			return $"{Id}\n{Name}\n{GPA}";
+		}
+	}
+}
